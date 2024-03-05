d2ca9d3b19314457b7b3fae2ccf534472b07930b
Hsuan <hsuanchang@star-link.tech>
2024-03-05 09:55:48 +0800

    Fix lint

--- lunch.js
+++ lunch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lunch
 // @namespace    http://tampermonkey.net/
-// @version      4.0
+// @version      4.1
 // @description  try to take over the world!
 // @author       You
 // @match        /^[^:/#?]*:\/\/([^#?/]*\.)?eats\.quickclick\.cc(:[0-9]{1,5})?\/.*$/
@@ -11,12 +11,12 @@
 // @updateURL    https://raw.githubusercontent.com/hsuan9522/Tampermonkey-Script/master/lunch.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
+;(function () {
+    'use strict'
 
     // Your code here...
-    if(location.href.match(/https:\/\/order\.ocard\.co\//g)) {
-        window.hs = async function() {
+    if (location.href.match(/https:\/\/order\.ocard\.co\//g)) {
+        window.hs = async function () {
             const order_id = location.href.replace('https://order.ocard.co/simplycarbs/order/', '')
 
             const formData = new FormData()
@@ -25,33 +25,34 @@
 
             const res = await fetch('https://api-order.ocard.co/order/get', {
                 method: 'POST',
-                body: formData
-            }).then(res => {
-                return res.json()
+                body: formData,
             })
-            .catch(err => {
-                console.log('err', err)
-            })
+                .then(res => {
+                    return res.json()
+                })
+                .catch(err => {
+                    console.log('err', err)
+                })
 
-            let total = Object.create(null);
+            let total = Object.create(null)
 
-            const ans = res.data.group.orders.reduce((arr,cur) => {
+            const ans = res.data.group.orders.reduce((arr, cur) => {
                 const user = cur.name
                 cur.items.forEach(e => {
                     let name = `${e.category_name}_${e.name}`
-                    if(e.adds.length>=1) {
+                    if (e.adds.length >= 1) {
                         name += '('
                         e.adds.forEach(el => {
                             name += el.name
                         })
                         name += ')'
                     }
-                    if(!arr[name]) {
+                    if (!arr[name]) {
                         arr[name] = []
                     }
                     arr[name].push(user)
 
-                    if(total[e.category_name]) {
+                    if (total[e.category_name]) {
                         total[e.category_name]++
                     } else {
                         total[e.category_name] = 1
@@ -65,38 +66,39 @@
             console.log('-----------Ocard-----------')
             console.log(ans)
             console.log('---------------------------')
-        };
+        }
 
         window.hs()
-
-    } else if(location.href.match(/https:\/\/eats\.quickclick\.cc\//g)) {
-        window.hs = async function() {
+    } else if (location.href.match(/https:\/\/eats\.quickclick\.cc\//g)) {
+        window.hs = async function () {
             const url = location.href.replace('line-orders', 'apis/orders')
-            const res = await fetch(url).then(res => {
-                return res.json()
-            }).catch(err => {
-                console.log('err',err)
-            })
+            const res = await fetch(url)
+                .then(res => {
+                    return res.json()
+                })
+                .catch(err => {
+                    console.log('err', err)
+                })
 
             const ans = res.products.reduce((arr, cur) => {
                 const name = cur.name.replace(/---(?:.[^\)]*)/g, '')
-                if(!arr[name]) {
+                if (!arr[name]) {
                     arr[name] = []
                 }
 
-                for(let i=0; i<cur.qty; i++) {
+                for (let i = 0; i < cur.qty; i++) {
                     arr[name].push(cur.name.match(/---((?:.[^\)]*))/g)[0].replace('---', ''))
                 }
 
                 return arr
             }, Object.create(null))
 
-            let total = Object.create(null);
-            for(let i in ans) {
+            let total = Object.create(null)
+            for (let i in ans) {
                 // console.log(i,x[i])
                 const name = i.replace(/( - .*)/g, '')
                 const count = ans[i].length
-                if(!total[name]) total[name] = 0
+                if (!total[name]) total[name] = 0
                 total[name] += count
             }
             total['0*Price'] = res.orderAmount
@@ -104,33 +106,35 @@
             console.log('-----------作燴-----------')
             console.log(ans)
             console.log('--------------------------')
-        };
+        }
 
         window.hs()
     }
-
-
 
     window.lunch = async (u1, u2) => {
         // 作燴兩單合併
         const url1 = u1.replace('line-orders', 'apis/orders')
         const url2 = u2.replace('line-orders', 'apis/orders')
 
-        const res1 = await fetch(url1).then(res => {
-            return res.json()
-        }).catch(err => {
-            console.log('err',err)
-        })
-        const res2 = await fetch(url2).then(res => {
-            return res.json()
-        }).catch(err => {
-            console.log('err',err)
-        })
+        const res1 = await fetch(url1)
+            .then(res => {
+                return res.json()
+            })
+            .catch(err => {
+                console.log('err', err)
+            })
+        const res2 = await fetch(url2)
+            .then(res => {
+                return res.json()
+            })
+            .catch(err => {
+                console.log('err', err)
+            })
 
         const res = res1.products.concat(res2.products)
         const ans = res.reduce((arr, cur) => {
             const name = cur.name.replace(/---(?:.[^\)]*)/g, '')
-            if(!arr[name]) {
+            if (!arr[name]) {
                 arr[name] = []
             }
 
@@ -138,12 +142,12 @@
             return arr
         }, Object.create(null))
 
-        let total = Object.create(null);
-        for(let i in ans) {
+        let total = Object.create(null)
+        for (let i in ans) {
             // console.log(i,x[i])
             const name = i.replace(/( - .*)/g, '')
             const count = ans[i].length
-            if(!total[name]) total[name] = 0
+            if (!total[name]) total[name] = 0
             total[name] += count
         }
 
@@ -155,4 +159,4 @@
         console.log(ans)
         console.log('--------------------------')
     }
-})();
+})()
